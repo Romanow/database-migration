c17161555b02e17af89802af33cee4af987e2456
Romanow <romanowalex@mail.ru>
2023-12-05 19:20:04 +0300

    v2.0

--- src/main/kotlin/ru/romanow/database/migration/domain/User.kt
+++ src/main/kotlin/ru/romanow/database/migration/domain/User.kt
@@ -2,6 +2,8 @@ package ru.romanow.database.migration.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
@@ -9,6 +11,7 @@ import jakarta.persistence.Table
 @Table(name = "users")
 data class User(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     var id: Int? = null,
 
